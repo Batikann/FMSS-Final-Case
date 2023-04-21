b6d9d47b4dd96862edc654b13b8687fb59bd018e
Batikann <emiruar123@gmail.com>
2023-04-21 22:53:43 +0300

    header component write css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -24,20 +24,48 @@ function Header() {
         alt="logo"
         className="w-52 h-44 object-center"
       />
-      <form className="flex items-center gap-4">
-        <input
-          type="text"
-          className="w-[400px] h-10 outline-none rounded-lg p-2 text-indigo-800"
-          onChange={(e) => setVal(e.target.value)}
-          value={val}
-          placeholder="Enter starship name or model"
-        />
-        <button
-          onClick={getDataBySearchValue}
-          className="bg-indigo-600 text-white flex items-center p-2 text-lg rounded-lg hover:scale-110"
+      <form className="w-[50%] hover:scale-110">
+        <label
+          for="search"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
-          Filter
-        </button>
+          Search
+        </label>
+        <div class="relative">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="search"
+            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search"
+            required
+            value={val}
+            onChange={(e) => setVal(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={getDataBySearchValue}
+            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
+        </div>
       </form>
     </div>
   )
